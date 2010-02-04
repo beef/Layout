a684d1f2189e5f74012b794ecd2c091a22cbb5fd
Daniel Craig <daniel@wearebeef.co.uk>
2010-02-04 14:23:14 +0000

    removed wireframes stuff

--- generators/layout_files/layout_files_generator.rb
+++ generators/layout_files/layout_files_generator.rb
@@ -2,9 +2,6 @@ class LayoutFilesGenerator < Rails::Generator::Base
   
   def manifest
     record do |m|
-      m.directory File.join("public", "stylesheets", "wireframe")
-      m.directory File.join("public", "stylesheets", "wireframe", "images")
-
       ["public/javascripts/application.js",
       "public/stylesheets/forms.css",
       "public/stylesheets/ie6.css",
@@ -12,24 +9,10 @@ class LayoutFilesGenerator < Rails::Generator::Base
       "public/stylesheets/layout.css",
       "public/stylesheets/mobile.css",
       "public/stylesheets/print.css",
+      "public/stylesheets/screen.css",
       "public/stylesheets/reset.css",
       "public/stylesheets/skin.css",
-      "public/stylesheets/typography.css",
-      "public/stylesheets/wireframe.css",
-      "public/stylesheets/wireframe/ie6.css",
-      "public/stylesheets/wireframe/ie7.css",
-      "public/stylesheets/wireframe/images/2nd_nav_bg.gif",
-      "public/stylesheets/wireframe/images/body_bg.gif",
-      "public/stylesheets/wireframe/images/content_wrap_bg.gif",
-      "public/stylesheets/wireframe/images/content_wrap_e_bg.gif",
-      "public/stylesheets/wireframe/images/featurebox2_bg.gif",
-      "public/stylesheets/wireframe/images/featurebox_bg.gif",
-      "public/stylesheets/wireframe/images/header_bg.gif",
-      "public/stylesheets/wireframe/images/sprites.gif",
-      "public/stylesheets/wireframe/images/td_bg.gif",
-      "public/stylesheets/wireframe/images/thumb_100wide.gif",
-      "public/stylesheets/wireframe/main.css",
-      "public/stylesheets/wireframe/print.css"].each do |file|
+      "public/stylesheets/typography.css"].each do |file|
          m.file file, file
       end
       
